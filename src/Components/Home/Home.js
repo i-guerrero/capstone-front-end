e7924ad69784e3afcf724d03075a027c03a8ddf0
SasukeDaTony <tonytaylor@pursuit.org>
2023-05-18 18:24:44 -0400

    home logo changed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,11 @@
 import "./Home.css";
 import welcomeF from "./welcomeF.png";
+import devteam from "./undraw_engineering_team_a7n2.svg";
 
 export default function Home() {
   return (
     <div className="home">
-      <img src={welcomeF} alt="welcomeF" />
+      <img src={devteam} alt="devteam" />
     </div>
   );
 }
