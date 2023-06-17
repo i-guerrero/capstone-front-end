f0ea75233ed1b4425be232838fb3f9e656ee9670
“danielsolaquegit config --global user.email “danielsolaque@pursuit.org <example@email.com>
2023-06-17 12:08:15 -0400

    addind layout to profile card

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -5,6 +5,10 @@ import "./ProfilePage.css";
 // import ConfirmationModal from "./Components/ConfirmationModal";
 import { getProposalByUserId } from "../API/Proposal";
 import ProposalCard from "./Components/ProposalCard";
+import { CiLocationOn } from "react-icons/ci";
+import { BsBuildingGear } from "react-icons/bs";
+import { AiOutlineMail, AiOutlineLinkedin } from "react-icons/ai";
+
 export default function ProfilePage({ firebaseToken, profileUser }) {
   const [proposals, setProposals] = useState([]);
 
@@ -17,69 +21,85 @@ export default function ProfilePage({ firebaseToken, profileUser }) {
     }
   }, [profileUser]);
 
-  console.log(proposals);
+  console.log(profileUser);
 
   if (!firebaseToken) return null;
 
   return (
     <>
       <div className="profile">
-        <img
-          src={profileUser.profile_img}
-          alt="profile_pic"
-          className="profile-pic"
-        />
-        <h1>
-          {profileUser.first_name} {profileUser.last_name}
-        </h1>
-        <br />
-        <h4>
-          {" "}
-          I am a {profileUser.user_type} Located In {profileUser.city},{" "}
-          {profileUser.country}
-        </h4>
+        <div className="profile-card">
+          <div className="profile-card-img-container">
+            <img
+              src={profileUser.profile_img}
+              alt="profile_pic"
+              className="profile-pic"
+            />
+          </div>
 
-        <br />
-        <h4>My organization is {profileUser.company}</h4>
-        <br />
-        <h4>
-          Email Me at <a href={profileUser.email}>{profileUser.email}</a>
-        </h4>
-        <br />
-        <a href={profileUser.linkedin}>
-          <h4>Check Out My Linkedin!</h4>
-        </a>
-        <hr />
-        {profileUser.user_type === "Mentor" ? (
-          <>
-            <u>
-              <h3>Proposals You Can Join!</h3> <hr />
-            </u>
-          </>
-        ) : null}
+          <header className="mb-3 text-center">
+            <h1 className="mb-0">
+              {profileUser.first_name} {profileUser.last_name}
+            </h1>
 
-        {profileUser.user_type === "Nonprofit" ? (
-          <>
-            <u>
-              <h3>My Proposal</h3> <hr />
-            </u>
-            {proposals[0] ? (
-              <ProposalCard
-                description={proposals[0].description}
-                title={proposals[0].title}
-                impact={proposals[0].impact}
-              />
+            <h4>{profileUser.user_type}</h4>
+          </header>
+
+          <div className="d-flex flex-column gap-3">
+            <div className="d-flex align-items-center gap-2">
+              <CiLocationOn size={32} />
+              <span className="h5 mb-0">
+                {profileUser.city} | {profileUser.country}
+              </span>
+            </div>
+
+            <div className="d-flex align-items-center gap-2">
+              <BsBuildingGear size={32} />
+              <span className="h5 mb-0">{profileUser.company}</span>
+            </div>
+
+            <div className="d-flex align-items-center gap-2">
+              <AiOutlineMail size={32} />
+              <span className="h5 mb-0">{profileUser.email}</span>
+            </div>
+
+            <div className="d-flex align-items-center gap-2">
+              <AiOutlineLinkedin size={32} />
+              <span className="h5 mb-0">{profileUser.linkedin}</span>
+            </div>
+
+            {profileUser.user_type === "Mentor" ? (
+              <>
+                <u>
+                  <h3>Proposals You Can Join!</h3>
+                </u>
+              </>
             ) : null}
-          </>
-        ) : null}
 
-        {profileUser.user_type === "Mentee" ? (
-          <>
-            <u>
-              <h3>Projects You Can Join!</h3> <hr />
-            </u>
-          </>
-        ) : null}
+            {profileUser.user_type === "Nonprofit" ? (
+              <>
+                <u>
+                  <h3>My Proposal</h3>
+                </u>
+                {proposals[0] ? (
+                  <ProposalCard
+                    description={proposals[0].description}
+                    title={proposals[0].title}
+                    impact={proposals[0].impact}
+                  />
+                ) : null}
+              </>
+            ) : null}
+
+            {profileUser.user_type === "Mentee" ? (
+              <>
+                <u>
+                  <h3>Projects You Can Join!</h3>
+                </u>
+              </>
+            ) : null}
+          </div>
+        </div>
       </div>
     </>
   );
